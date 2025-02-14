c194c85d388e147bb4475e9111d64dee49817592
Oksana Baragicheva <minzhulinao@mail.ru>
2025-02-14 16:47:35 +0700

    Добавила генерацию уникального ID фото, URL фото и описания

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Проверяет длину строки
-function checkLengthString (string, Maxlength) {
+function checkLengthString(string, Maxlength) {
   return string.length <= Maxlength;
 }
 
@@ -11,71 +11,60 @@ checkLengthString('', 140);
 // likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
 // comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение. Все комментарии генерируются случайным образом.
 
-const ID_PHOTOS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-
-// const USL_PHOTO = [
-//   'photos/1.jpg',
-//   'photos/2.jpg',
-//   'photos/3.jpg',
-// ];
-
-const DESCRIPTION_PHOTO = [
+const DESCRIPTION_PHOTO_WHAT = [
   'закат',
   'рассвет',
   'горы',
   'дерево',
-  'дом',
   'котики',
   'собака',
   'мой завтрак',
   'прогулка',
   'цветочки',
-  'улица',
-  'новое авто',
   'любимое кафе',
-  'релакс',
-  'ночь',
-  'луна',
   'первй снег',
-  'примерка',
-  'отпуск',
-  'море',
-  'на пляже',
-  'встреча',
-  'на катке',
-  'рыбалка',
   'ужин',
+];
+
+const DESCRIPTION_PHOTO_WHERE = [
+  'на улице',
+  'ночью',
+  'в отпуске',
+  'на море',
+  'на пляже',
+  'на катке',
+  'у моего дома',
 ];
 
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
 
-// const COMMENTS = [
-//   '',
-//   '',
-//   '',
-// ];
+const SIMILAR_DESCRIPTION_COUNT = 25;
 
 // Возвращает случайное целое число из диапазона
-function getRandomPositiveInteger (a, b) {
+const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-}
+};
 
 // Возвращает случайный элемент массива
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
+// Генерирует ID фотогрфи
+let lastUsedId = 1;
+let lastUsedUrlNamber = 1;
+
 const createDescriptionPhoto = () => ({
-  idPhoto: getRandomArrayElement(ID_PHOTOS),
-  urlPhoto: `photos/${  getRandomArrayElement(ID_PHOTOS)  }.jpg`,
-  descriptionPhoto: getRandomArrayElement(DESCRIPTION_PHOTO),
+  idPhoto: lastUsedId++,
+  urlPhoto: `photos/${lastUsedUrlNamber++}.jpg`,
+  descriptionPhoto: `${getRandomArrayElement(DESCRIPTION_PHOTO_WHAT)} ${getRandomArrayElement(DESCRIPTION_PHOTO_WHERE)}`,
   likesPhoto: getRandomPositiveInteger(MIN_LIKES, MAX_LIKES),
-  commentsPhoto: 1,
+  commentsPhoto: [],
 });
 
-const similarDescriptionPhoto = Array.from({length: 25}, createDescriptionPhoto);
+const similarDescriptionPhoto = Array.from({ length: SIMILAR_DESCRIPTION_COUNT }, createDescriptionPhoto);
 
 getRandomPositiveInteger(2, 15);
-similarDescriptionPhoto();
+console.log(similarDescriptionPhoto);
